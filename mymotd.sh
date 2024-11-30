f286101f8f4087d07c77457a233dba49a5bc6917
Marin N. <marinnedea@users.noreply.github.com>
2024-11-30 23:05:32 +0200

    Update mymotd.sh
    - Replaced grep and awk pipelines with single awk commands for efficiency.
    - Used built-in variables like $USER and $PWD instead of invoking external commands.
    - Handled potential absence of GPU information gracefully.
    - Simplified memory and swap usage calculations.
    - Ensured all commands handle unexpected output or errors appropriately.

--- mymotd.sh
+++ mymotd.sh
@@ -1,57 +1,53 @@
 #!/bin/sh
-#
+
+# Clear the screen
 clear
 
+# Gather system information
+os_release=$(hostnamectl | awk -F': ' '/Operating System/ {print $2}')
+host_name=$(hostnamectl | awk -F': ' '/Hostname/ {print $2}')
+kernel_vers=$(uname -r)
+iam=$USER
+mywd=$PWD
+shellinfo=$(basename "$SHELL")
+shellvers=$("$SHELL" --version 2>/dev/null | awk '/version/ {print $NF}' | head -n1)
+terminfo=$(tty)
+memory=$(free -mh | awk '/Mem:/ {print $3 " / " $2}')
+cpu=$(awk -F': ' '/model name/ {print $2; exit}' /proc/cpuinfo)
+gpu=$(lspci | awk -F': ' '/VGA compatible controller/ {print $3}')
+showup=$(uptime -p | cut -d ' ' -f2-)
+
+# Display system information
 printf "\n"
-printf "\n"
+printf "\e[1;34mOS:\e[0m %s\n" "$os_release"
+printf "\e[1;34mHostname:\e[0m %s\n" "$host_name"
+printf "\e[1;34mKernel:\e[0m %s\n" "$kernel_vers"
+printf "\e[1;34mUptime:\e[0m %s\n" "$showup"
+printf "\e[1;34mYou are logged in as:\e[0m %s\n" "$iam"
+printf "\e[1;34mYour working directory is:\e[0m %s\n" "$mywd"
+printf "\e[1;34mShell:\e[0m %s %s\n" "$shellinfo" "$shellvers"
+printf "\e[1;34mTerminal:\e[0m %s\n" "$terminfo"
+printf "\e[1;34mCPU:\e[0m %s\n" "$cpu"
+[ -n "$gpu" ] && printf "\e[1;34mGPU:\e[0m %s\n" "$gpu"
+printf "\e[1;34mMemory:\e[0m %s\n" "$memory"
 
-os_release="$(hostnamectl | grep -i operating | awk -F':' '{print $2}')"
-host_name="$(hostnamectl | grep -i hostname | awk -F':' '{print $2}')"
-kernel_vers="$(hostnamectl | grep -i kernel | awk -F':' '{print $2}')"
-iam="$(whoami)"
-mywd="$(pwd)"
-shellinfo="$(echo $SHELL | awk -F'/' '{print $3}')"
-shellinfo="${shellinfo^}"
-terminfo="$(tty)"
-shellvers="$($SHELL --version  | grep -i release | awk '{print $4}' | awk -F'-' '{ print $1 }')"
-memory="$(free -mh | grep Mem | awk '{print $3 " / "  $2}')"
-cpu="$(lscpu | grep -i "model name" | awk -F':' '{print $2}' | tr -d '[:space:]')')"
-gpu="$(lspci | grep VGA | cut -d ":" -f3)"
-showup="$(uptime -p | awk '{$1="" ;print}')"
+# Additional system information
+date=$(date)
+load=$(awk '{print $1}' /proc/loadavg)
+root_usage=$(df -h / | awk 'NR==2 {print $(NF-1)}')
+memory_usage=$(free -m | awk '/Mem:/ {printf "%.1f%%", $3/$2*100}')
+swap_usage=$(free -m | awk '/Swap:/ {printf "%.1f%%", $3/$2*100}')
+users=$(users | wc -w)
+time=$(uptime -p | cut -d ' ' -f2-)
+processes=$(ps -e --no-headers | wc -l)
+ip=$(hostname -I | awk '{print $1}')
 
-echo -e "\e[1;34mOS:\e[0m $os_release" 
-echo -e "\e[1;34mHostname:\e[0m $host_name"
-echo -e "\e[1;34mKernel:\e[0m $kernel_vers"
-#echo -e "Connected users:"
-#w
-echo -e "\e[1;34mUptime:\e[0m $showup"
-echo -e "\e[1;34mYou are logged in as:\e[0m $iam"
-echo -e "\e[1;34mYour working directory is:\e[0m $mywd"
-echo -e "\e[1;34mShell:\e[0m $shellinfo $shellvers"    
-echo -e "\e[1;34mTerminal:\e[0m $terminfo"
-echo -e "\e[1;34mCPU:\e[0m $cpu"
-echo -e "\e[1;34mGPU:\e[0m $gpu"
-echo -e "\e[1;34mMemory:\e[0m $memory"
+# Display additional system information
+printf "\nSystem information as of: %s\n" "$date"
+printf "System load:\t%s\tIP Address:\t%s\n" "$load" "$ip"
+printf "Memory usage:\t%s\tSystem uptime:\t%s\n" "$memory_usage" "$time"
+printf "Usage on /:\t%s\tSwap usage:\t%s\n" "$root_usage" "$swap_usage"
+printf "Local Users:\t%s\tProcesses:\t%s\n" "$users" "$processes"
 
-
-
-date=`date`
-load=`cat /proc/loadavg | awk '{print $1}'`
-root_usage=`df -h / | awk '/\// {print $(NF-1)}'`
-memory_usage=`free -m | awk '/Mem:/ { total=$2 } /buffers\/cache/ { used=$3 } END { printf("%3.1f%%", used/total*100)}'`
-swap_usage=`free -m | awk '/Swap/ { printf("%3.1f%%", "exit !$2;$3/$2*100") }'`
-users=`users | wc -w`
-time=`uptime | grep -ohe 'up .*' | sed 's/,/\ hours/g' | awk '{ printf $2" "$3 }'`
-processes=`ps aux | wc -l`
-ethup=$(ip -4 ad | grep 'state UP' | awk -F ":" '!/^[0-9]*: ?lo/ {print $2}')
-ip=$(ip ad show dev $ethup |grep -v inet6 | grep inet|awk '{print $2}')
-
-echo "System information as of: $date"
-echo
-printf "System load:\t%s\tIP Address:\t%s\n" $load $ip
-printf "Memory usage:\t%s\tSystem uptime:\t%s\n" $memory_usage "$time"
-printf "Usage on /:\t%s\tSwap usage:\t%s\n" $root_usage $swap_usage
-printf "Local Users:\t%s\tProcesses:\t%s\n" $users $processes
-echo
-
-[ -f /etc/motd.tail ] && cat /etc/motd.tail || true
+# Display /etc/motd.tail if it exists
+[ -f /etc/motd.tail ] && cat /etc/motd.tail
